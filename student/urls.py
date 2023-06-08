bb4a3e245f7a345c3703651399815901d2d27eda
fidaaaaa <fidakummali@gmail.com>
2023-06-08 09:50:48 +0530

    food form edited

--- student/urls.py
+++ student/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("web.urls", namespace="web")),
-    # path('food/', include("food.urls", namespace="food")),
+    path('food/', include("food.urls", namespace="food")),
     # path('users/', include("users.urls", namespace="users")),
 ]

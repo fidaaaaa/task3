c2d0dce892f21d5e6bfb6552579d8c4623e0f507
fidaaaaa <fidakummali@gmail.com>
2023-06-08 12:11:50 +0530

    models edited

---
+++ student/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+from django.contrib.auth.models import User
+
+
+
+class Student(models.Model):
+    number = models.IntegerField()
+    full_name = models.CharField(max_length=255)
+    phone = models.IntegerField()
+    email = models.EmailField()
+    password = models.CharField(max_length=255)
+    user =models.OneToOneField("auth.User",on_delete=models.CASCADE)
+    food = models.ManyToManyField("food.Food")
+    
+
+
+    def __str__(self):
+        return self.full_name
--- student/urls.py
+++ student/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("web.urls", namespace="web")),
     path('food/', include("food.urls", namespace="food")),
-    # path('users/', include("users.urls", namespace="users")),
+    path('users/', include("users.urls", namespace="users")),
 ]
